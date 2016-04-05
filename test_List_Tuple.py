541b6e3c249b40573f05dbca30b9a46752e7f5b5
HolaWby <wubuyuen@sina.com>
2016-04-05 15:58:25 +0800

    add test_List_Tuple.py

---
+++ test_List_Tuple.py
@@ -0,0 +1,8 @@
+L=[
+  ['Apple','Google','Microsoft'],
+  ['Java','Python','Rudy','PHP'],
+  ['Adam','Bart','Lisa']
+]
+print('打印Apple:',L[0][0])
+print('打印Python:%s'% L[1][1])
+print('打印Lisa:%s' % L[2][2])
